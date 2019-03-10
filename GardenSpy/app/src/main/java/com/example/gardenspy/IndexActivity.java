be328cbf73f9bac029f069d7bcab9cf67b8285f8
Thais <tdesouza@uncc.edu>
2019-03-10 11:10:59 -0400

    Index activity start page, added contraints to xmls

--- GardenSpy/app/src/main/java/com/example/gardenspy/IndexActivity.java
+++ GardenSpy/app/src/main/java/com/example/gardenspy/IndexActivity.java
@@ -1,7 +1,9 @@
 package com.example.gardenspy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class IndexActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class IndexActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+    }
+
+    public void openLoginPage(View view){
+        Intent newActivity = new Intent(this, LoginActivity.class);
+        startActivity(newActivity);
+    }
+
+    public void openSignupPage(View view){
+        Intent newActivity = new Intent(this, SignupActivity.class);
+        startActivity(newActivity);
     }
 }
