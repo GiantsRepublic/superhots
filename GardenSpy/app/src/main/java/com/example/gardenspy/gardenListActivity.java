1c02df86dfb29e9de072de430eb9afb0134e7917
kwei <kwei@uncc.edu>
2019-03-11 00:29:00 -0400

    added garden list screen with empty table layout

---
+++ GardenSpy/app/src/main/java/com/example/gardenspy/gardenListActivity.java
@@ -0,0 +1,13 @@
+package com.example.gardenspy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gardenListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garden_list);
+    }
+}
