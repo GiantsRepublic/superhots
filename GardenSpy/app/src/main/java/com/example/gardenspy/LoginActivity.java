f2fc4962c09d7e6161b5b3c561f5c5530df87dce
leoneljerez <leoneljerez1022@gmail.com>
2019-03-11 23:18:01 -0400

    Cleaner, Working Login/SignUp Code.

--- GardenSpy/app/src/main/java/com/example/gardenspy/LoginActivity.java
+++ GardenSpy/app/src/main/java/com/example/gardenspy/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.gardenspy;
 
+import android.content.Intent;
 import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,38 +20,44 @@ import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private static final String TAG = "LoginActivity";
+    private EditText emailEdit, passwordEdit;
+    private Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Initialize Firebase Authentication
+
+        emailEdit = findViewById(R.id.editTextEmail);
+        passwordEdit = findViewById(R.id.editTextPassword);
+        signin = findViewById(R.id.buttonSignin);
+
         mAuth = FirebaseAuth.getInstance();
-    }
-    @Override
-    public void onStart(){
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
 
-        EditText email = findViewById(R.id.editText);
-        EditText password = findViewById(R.id.editText2);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEdit.getText().toString().trim();
+                String password = passwordEdit.getText().toString().trim();
 
+                /*
+                Write checks for login here
+                 */
 
-        mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //updateUI(user);
-                        } else {
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
-                        }
-                    }
-                });
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    startActivity(new Intent(getApplicationContext(),gardenListActivity.class));
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(getApplicationContext(),"Authentication Error", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
--- GardenSpy/app/src/main/java/com/example/gardenspy/SignupActivity.java
+++ GardenSpy/app/src/main/java/com/example/gardenspy/SignupActivity.java
@@ -1,11 +1,14 @@
 package com.example.gardenspy;
 
+import android.content.Intent;
 import android.nfc.Tag;
 import android.nfc.TagLostException;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,43 +21,44 @@ import com.google.firebase.auth.FirebaseUser;
 public class SignupActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private static final String TAG = "SignupActivity";
+    private EditText emailEdit, passwordEdit;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        // Initialize Firebase Authentication
+
+        emailEdit = findViewById(R.id.editTextEmail);
+        passwordEdit = findViewById(R.id.editTextPassword);
+        register = findViewById(R.id.buttonSignup);
+
         mAuth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEdit.getText().toString().trim();
+                String password = passwordEdit.getText().toString().trim();
+
+                /*
+                Write checks here for password requirements
+                 */
+
+                mAuth.createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(getApplicationContext(), "Email or password is wrong",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
-
-        EditText email = findViewById(R.id.editText4);
-        EditText password = findViewById(R.id.editText5);
-
-        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(SignupActivity.this, "Authentication Failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
-                        }
-
-                    }
-                });
-    }
-
-
 }
