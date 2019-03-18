526fe068d26a6196981b4574a5e766b710782763
leoneljerez <leoneljerez1022@gmail.com>
2019-03-18 10:51:09 -0400

    Added temporary buttons so everyone can test the details and garden list layouts

--- GardenSpy/app/src/main/java/com/example/gardenspy/gardenListActivity.java
+++ GardenSpy/app/src/main/java/com/example/gardenspy/gardenListActivity.java
@@ -1,7 +1,10 @@
 package com.example.gardenspy;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class gardenListActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class gardenListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garden_list);
+    }
+
+
+    public void openDetailsPage(View view){
+        Intent newActivity = new Intent(this, DetailsActivity.class);
+        startActivity(newActivity);
     }
 }
