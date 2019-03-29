7536e1a6391d2b6cec477d7a6efc50e92f8130ba
kwei <kwei@uncc.edu>
2019-03-29 16:48:23 -0400

    trying to access data, failed

--- GardenSpy/app/src/main/java/com/example/gardenspy/DetailsActivity.java
+++ GardenSpy/app/src/main/java/com/example/gardenspy/DetailsActivity.java
@@ -1,13 +1,57 @@
 package com.example.gardenspy;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+//import com.google.firebase.auth.Firebase
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private static final String TAG = "DetailsActivity";
+    private FirebaseDatabase mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        //setup text labels
+        TextView timeLabel = (TextView) findViewById(R.id.timeLabel);
+        TextView vaporLabel = (TextView) findViewById(R.id.vaporLabel);
+        TextView dewLabel = (TextView) findViewById(R.id.dewLabel);
+        TextView moistLabel = (TextView) findViewById(R.id.moistLabel);
+        TextView humLabel = (TextView) findViewById(R.id.humLabel);
+        TextView satVapLabel = (TextView) findViewById(R.id.satVapLabel);
+        TextView tempLabel = (TextView) findViewById(R.id.timeLabel);
+
+        timeLabel.setText("hello");
+
+        //creating database instance
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference rootRef = database.getReference("test-5487a/user/key/plants/reaper");
+        mRef = new Firebase("https://test-5487a.firebaseio.com/");
+
+        rootRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String vapor = dataSnapshot.getValue(String.class);
+                System.out.println(vapor);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("error");
+            }
+        });
+
     }
 }
