103bb08d12d81e06688797de576ddc6a083ade67
TheRealDuvy <TheRealDuvy@Gmail.com>
2019-04-09 17:33:37 -0400

    Cleaned code and turned off thresholds for now

--- GardenPi/Alpha_Code/Threshold.py
+++ GardenPi/Alpha_Code/Threshold.py
@@ -2,29 +2,46 @@
 # Luke Duvall
 # Initiate Thresholds and Check/Set Threshold Changes
 
-# Import Firebase library
-#from firebase import firebase
-
-# Firebase Database Reference
-#fb = firebase.FirebaseApplication('https://test-5487a.firebaseio.com/', None)
-
 class Threshold:
     
     def __init__(self):
         
         self.temp_threshold = 85
-        self.humid_threshold = 40
+        self.humid_threshold = 60
         self.moist_threshold = 25
         self.time_on = 7
         self.time_off = 19
         
-    #def checkThreshold(self,fb):
+    def setThreshold(self,fb):
         
-        #self.temp_threshold = fb.get('/user/key/plants/reaper/temp', 'threshold')
-        #self.humid_threshold = fb.get('/user/key/plants/reaper/humid', 'threshold')
-        #self.moist_threshold = fb.get('/user/key/plants/reaper/moisture', 'threshold')
-        #self.time_on = fb.get('/user/key/plants/reaper/timer', 'light_on' )
-        #self.time_off = fb.get('/user/key/plants/reaper/timer', 'light_off')
+        if fb.get('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/temp', 'threshold') is not None:
+            self.temp_threshold = fb.get('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/temp', 'threshold')
+        else:
+            temp_thresh_fb = fb.patch('user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/temp', {'threshold':self.temp_threshold})
+        
+            
+        if fb.get('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/humid', 'threshold') is not None:
+            self.humid_threshold = fb.get('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/humid', 'threshold')
+        else:
+            humid_thresh_fb = fb.patch('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/humid', {'threshold':self.humid_threshold})
+            
+            
+        if fb.get('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/moisture', 'threshold') is not None:
+            self.moist_threshold = fb.get('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/moisture', 'threshold')
+        else:
+            moist_thresh_fb = fb.patch('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/moisture', {'threshold':self.moist_threshold})
+            
+            
+        if fb.get('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/timer', 'light_on') is not None:
+            self.time_on = fb.get('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/timer', 'light_on')
+        else:
+            time_on_fb = fb.patch('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/timer', {'light_on':self.time_on})
+            
+            
+        if fb.get('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/timer', 'light_off') is not None:
+            self.time_off = fb.get('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/timer', 'light_off')
+        else:
+            time_off_fb = fb.patch('/user/xD67KpdT7YgZ9qEVv8BrmIAccZ53/plants/reaper/timer', {'light_off':self.time_off})
         
     def getTempThresh(self):
         
